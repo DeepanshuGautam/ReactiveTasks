e423d80ae07c548576ce4a94544526e38d854555
Deepanshu Gautam <gtmdeepanshu@gmail.com>
2018-12-21 17:00:46 +0900

    Add viewController code

--- ReactiveTasks/ViewController.swift
+++ ReactiveTasks/ViewController.swift
@@ -6,15 +6,38 @@
 //  Copyright © 2018 deepanshugautam. All rights reserved.
 //
 
-import UIKit
+import ReactiveSwift
+import Result
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        let signal = Signal<String,NoError> { (observer, lifetime) in
+            /*
+             DispatchQueue is used because, signal starts immediately after initialization,
+             so to observe it in time, have to delay the signal, in this case by 1 second
+             */
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+                observer.send(value: "String 1")
+                observer.send(value: "String 2")
+                observer.sendCompleted()
+            }
+        }
+
+        signal.observeValues { string in
+            print(string)
+        }
+
+        let signalProducer = SignalProducer<Int, NoError> { (observer, _) in
+            observer.send(value: 1)
+            observer.send(value: 2)
+            observer.sendCompleted()
+        }
+
+        signalProducer.startWithValues { int in
+            print(int)
+        }
     }
-
-
 }
-
