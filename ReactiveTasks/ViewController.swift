2a4c4115389db413ebbef94f29ad261ca583a8b3
Deepanshu Gautam <gtmdeepanshu@gmail.com>
2018-12-21 17:38:42 +0900

    Add viewController code

--- ReactiveTasks/ViewController.swift
+++ ReactiveTasks/ViewController.swift
@@ -6,13 +6,18 @@
 //  Copyright © 2018 deepanshugautam. All rights reserved.
 //
 
-import UIKit
+import ReactiveSwift
+import Result
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let inputValues = ["ios", "mobile", "android", "mercari", "tokyo"]
+        let signalProducer = SignalProducer<String, NoError>.init(inputValues)
+        signalProducer
+            .filter { $0.count > 6 }
+            .startWithValues { print($0) }
     }
 
 
