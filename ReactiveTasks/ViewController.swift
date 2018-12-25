541a988c7e3e4987c9dfecfed07ae5a9029ba1d8
Deepanshu Gautam <gtmdeepanshu@gmail.com>
2018-12-25 17:33:42 +0900

    Add viewController code

--- ReactiveTasks/ViewController.swift
+++ ReactiveTasks/ViewController.swift
@@ -6,15 +6,25 @@
 //  Copyright © 2018 deepanshugautam. All rights reserved.
 //
 
-import UIKit
+import ReactiveCocoa
+import ReactiveSwift
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var textField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        label.reactive.text <~ textField.reactive.continuousTextValues
+        label.reactive.textColor <~ textField.reactive.continuousTextValues.filterMap {
+            guard let value = $0 else { return nil }
+            if value.count > 5 && value.count < 10 {
+                return UIColor.green
+            } else {
+                return UIColor.red
+            }
+        }
     }
-
-
 }
 
